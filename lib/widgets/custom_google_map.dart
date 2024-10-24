65a1ba32240ad3ebe739562d574eb1c488a15ba5
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-10-24 17:29:11 +0300

    -Change position -Google map controller -Camera update

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -10,6 +10,7 @@ class CustomGoogleMap extends StatefulWidget {
 
 class _CustomGoogleMapState extends State<CustomGoogleMap> {
   late CameraPosition initialCameraPosition;
+  late GoogleMapController googleMapController;
 
   @override
   void initState() {
@@ -21,18 +22,45 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   }
 
   @override
+  void dispose() {
+    googleMapController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           GoogleMap(
+            onMapCreated: (contrller) {
+              googleMapController = contrller;
+            },
             initialCameraPosition: initialCameraPosition,
             cameraTargetBounds: CameraTargetBounds(
               LatLngBounds(
-                  southwest:
-                      const LatLng(31.080569617326795, 29.763491041232577),
-                  northeast:
-                      const LatLng(31.30846738149212, 30.169298507189573)),
+                  southwest: const LatLng(31.08, 29.76),
+                  northeast: const LatLng(31.30, 30.16)),
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            left: 16,
+            child: ElevatedButton(
+              onPressed: () {
+
+                googleMapController.animateCamera(
+                    CameraUpdate.newLatLng(const LatLng(31.18, 29.92)));
+                // CameraPosition newLocation = const CameraPosition(
+                //   target: LatLng(31.18, 29.92),
+                //   zoom: 12,
+                // );
+                // googleMapController.animateCamera(
+                //   CameraUpdate.newCameraPosition(newLocation),
+                // );
+              },
+              child: const Text('Change location'),
             ),
           ),
         ],
