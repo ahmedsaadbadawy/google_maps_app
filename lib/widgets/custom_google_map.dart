5b0648a500315591f9cef00131f58ba61192317c
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-10-24 17:09:25 +0300

    -Display google maps. -Camera position. -Zoom level. -Camera bounds.

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class CustomGoogleMap extends StatefulWidget {
+  const CustomGoogleMap({super.key});
+
+  @override
+  State<CustomGoogleMap> createState() => _CustomGoogleMapState();
+}
+
+class _CustomGoogleMapState extends State<CustomGoogleMap> {
+  late CameraPosition initialCameraPosition;
+
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(31.187084851056554, 29.928110526889437),
+      zoom: 12,
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          GoogleMap(
+            initialCameraPosition: initialCameraPosition,
+            cameraTargetBounds: CameraTargetBounds(
+              LatLngBounds(
+                  southwest:
+                      const LatLng(31.080569617326795, 29.763491041232577),
+                  northeast:
+                      const LatLng(31.30846738149212, 30.169298507189573)),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+// world view 0 -> 3
+// country view 4 -> 6
+// city view 10 -> 12
+// street view 13 -> 17
+// building view 18 -> 20
